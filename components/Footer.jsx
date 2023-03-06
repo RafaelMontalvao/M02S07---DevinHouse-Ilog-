8de521c7feb4bcf22c23166e38a9d46123fa7ff3
Rafael Brito <rushbig@hotmail.com>
2023-03-06 16:44:55 -0300

    exercicio 3

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+function Footer() {
+    return (
+      <header className="Footer">
+        <div className="info">
+            <p>DEVINCursos 2023</p>
+        </div>
+      </header>
+    )
+  }
+  
+  export default Footer;
---
+++ pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div className="Home">
+      <Navbar />;
+      <Footer />;
+
+      
+    </div>
+  )
+}
+
+export default Home;
